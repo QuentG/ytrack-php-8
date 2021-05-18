cb1c8d5b2ceb90233495f54c4c6a159ae8e46e29
qgans <qgans@clever-age.com>
2021-05-18 17:52:43 +0200

    Add introduction excercise + use ReflectionFunction for explode && join exercise

--- php/tests/explode_test.php
+++ php/tests/explode_test.php
@@ -2,6 +2,12 @@
 
 $tests = [];
 
+$reflection = new ReflectionFunction('explodeWords');
+
+$tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfParameters(), 3);
+$tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfRequiredParameters(), 1);
+$tests[] = static fn ($eq): bool => $eq($reflection->hasReturnType(), true);
+
 $tests[] = static fn ($eq): bool => $eq(gettype(explodeWords("Hello there")), 'array');
 $tests[] = static fn ($eq): bool => $eq(gettype(explodeWords("Hello-there", "-")), 'array');
 $tests[] = static fn ($eq): bool => $eq(gettype(explodeWords("Hello-there", "-", -1)), 'array');
@@ -15,4 +21,3 @@ $tests[] = static fn ($eq): bool => !$eq(explodeWords('La-fonction/explode', "-"
 $tests[] = static fn ($eq): bool => $eq(explodeWords('La-fonction/explode/est/trop-bien', "/", 2), ['La-fonction', 'explode/est/trop-bien']);
 $tests[] = static fn ($eq): bool => $eq(explodeWords('La-fonction/explode/est/trop-bien', "/", -1), ['La-fonction', 'explode', 'est']);
 $tests[] = static fn ($eq): bool => $eq(count(explodeWords('La-fonction/explode/est/trop-bien', "/", -1)), 3);
-
--- php/tests/join_test.php
+++ php/tests/join_test.php
@@ -2,6 +2,11 @@
 
 $tests = [];
 
+$reflection = new ReflectionFunction('joinWords');
+
+$tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfParameters(), 2);
+$tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfRequiredParameters(), 1);
+$tests[] = static fn ($eq): bool => $eq($reflection->hasReturnType(), true);
 $tests[] = static fn ($eq): bool => $eq(gettype(joinWords([])), 'string');
 $tests[] = static fn ($eq): bool => $eq(gettype(joinWords([], "-")), 'string');
 
